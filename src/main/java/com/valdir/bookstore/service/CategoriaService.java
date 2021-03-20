558dcf206ff4fe955454bda30b8a8109290a6144
nishankRadicleInc <nsinghal@radicleinc.com>
2021-03-20 15:51:46 +0530

    first commit

---
+++ src/main/java/com/valdir/bookstore/service/CategoriaService.java
@@ -0,0 +1,53 @@
+package com.valdir.bookstore.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import com.valdir.bookstore.domain.Categoria;
+import com.valdir.bookstore.dtos.CategoriaDTO;
+import com.valdir.bookstore.repositories.CategoriaRepository;
+import com.valdir.bookstore.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repository;
+
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public List<Categoria> findAll() {
+		return repository.findAll();
+	}
+
+	public Categoria create(Categoria obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
+		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new com.valdir.bookstore.service.exceptions.DataIntegrityViolationException(
+					"Categoria não pode ser deletada! Possui livros associados");
+		}
+	}
+
+}
